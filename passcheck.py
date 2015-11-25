249353ba2adef5d7b0feb72469aca6ceb93c481d
Mattias Andrée <maandree@operamail.com>
2015-11-25 06:01:45 +0100

    Permission granted to redistribute under the 3-clause BSD license
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- passcheck.py
+++ passcheck.py
@@ -7,7 +7,8 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# (at your option) any later version, or under the New BSD LICENSE
+# as published by the Regents of the University of California.
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
